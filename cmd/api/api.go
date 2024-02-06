bcba4ac6d0f2ea646022c552d2f60246b3180283
Travis Aucoin <travisdevaucoin@gmail.com>
2024-02-06 16:05:17 -0600

    Add huma/v2 and go-chi/chi/v5 dependencies to improve API functionality
    New dependencies huma/v2 and go-chi/chi/v5 packages for API management were added into go.mod file. A new api.go file was also established to integrate these packages, allowing us to create and enqueue job records. Further changes include renaming main.go to default/main.go and updating numerous dependencies.

---
+++ cmd/api/api.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/danielgtaylor/huma/v2"
+	"github.com/danielgtaylor/huma/v2/adapters/humachi"
+	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
+	"github.com/rs/zerolog"
+	"github.com/tnaucoin/Janus/config"
+	"github.com/tnaucoin/Janus/internal/dynamo"
+	"github.com/tnaucoin/Janus/models/QueueRecord"
+	"net/http"
+	"os"
+)
+
+var (
+	localEnvFile = "local.env"
+	workersCount = 10
+	chName       = "job"
+	baseApiPath  = "/api/v1"
+)
+
+type CreateJobRecordInput struct {
+	Body struct {
+		Priority string `json:"priority,required" enum:"NONE,LOW,MEDIUM,HIGH"`
+	}
+}
+type CreateJobRecordOutput struct {
+	Body struct {
+		JobID string `json:"job_id" example:"803d73fa-1dd7-41c4-8b03-5125f1b5446c" doc:"The resulting job-id"`
+	}
+}
+type JobEnqueueInput struct {
+	Body struct {
+		JobIDs         []string `json:"job_ids,required" example:"dc08db82-ca8b-44bc-be55-c5b36cd774f4,803d73fa-1dd7-41c4-8b03-5125f1b5446c" doc:"The job_ids to enqueue"`
+		QueuePartition int      `json:"queue_partition,required" example:"1" doc:"The queue partition to enqueue the record into" default:"1"`
+	}
+}
+
+type JobEnqueueOutput struct{}
+
+func main() {
+	logger, _, ddbConnection := Init()
+	router := chi.NewMux()
+	api := humachi.New(router, huma.DefaultConfig("Janus", "1.0.0"))
+	huma.Register(api, huma.Operation{
+		OperationID:   "create-job-record",
+		Summary:       "Create a Janus Job Record Entry",
+		Method:        http.MethodPost,
+		Path:          fmt.Sprintf("%s/job", baseApiPath),
+		DefaultStatus: http.StatusCreated,
+	}, func(ctx context.Context, input *CreateJobRecordInput) (*CreateJobRecordOutput, error) {
+		resp := &CreateJobRecordOutput{}
+		var priority QueueRecord.QPriority
+		switch input.Body.Priority {
+		case "NONE":
+			priority = QueueRecord.NONE
+		case "LOW":
+			priority = QueueRecord.LOW
+		case "MEDIUM":
+			priority = QueueRecord.MEDIUM
+		case "HIGH":
+			priority = QueueRecord.HIGH
+		}
+		logger.Info().Str("Priority", input.Body.Priority).Int64("Value", int64(priority)).Msg("")
+		record := QueueRecord.NewQRecord(priority)
+		if err := ddbConnection.AddRecord(record); err != nil {
+			logger.Err(err).Msg("failed to add record to dynamo")
+			return nil, err
+		}
+		resp.Body.JobID = record.Id
+		return resp, nil
+	})
+	huma.Register(api, huma.Operation{
+		OperationID:   "enqueue-job-records",
+		Summary:       "Enqueue Janus Job Records",
+		Method:        http.MethodPost,
+		Path:          fmt.Sprintf("%s/enqueue", baseApiPath),
+		DefaultStatus: http.StatusAccepted,
+	}, func(ctx context.Context, input *JobEnqueueInput) (*JobEnqueueOutput, error) {
+		resp := &JobEnqueueOutput{}
+		for _, jobId := range input.Body.JobIDs {
+			if err := ddbConnection.EnqueueRecord(jobId, input.Body.QueuePartition); err != nil {
+				logger.Err(err).Msgf("failed to enqueue record: %s", jobId)
+				return nil, err
+			}
+		}
+		return resp, nil
+	})
+	logger.Info().Msg("api running.")
+	http.ListenAndServe("127.0.0.1:8001", router)
+}
+func Init() (zerolog.Logger, *config.Conf, *dynamo.DDBConnection) {
+	_ = godotenv.Load(localEnvFile)
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	logger := zerolog.New(zerolog.ConsoleWriter{Out: os.Stderr}).With().Timestamp().Logger()
+	conf := config.New()
+	ddbclient, err := dynamo.New(*conf, logger)
+	if err != nil {
+		logger.Fatal().Err(err)
+	}
+	if conf.App.Env == "development" {
+		err = dynamo.CreateLocalTable(ddbclient)
+		if err != nil {
+			logger.Fatal().Err(err).Msg("failed to create the ddb table")
+		}
+	}
+	return logger, conf, ddbclient
+}
--- go.mod
+++ go.mod
@@ -9,6 +9,8 @@ require (
 	github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue v1.12.16
 	github.com/aws/aws-sdk-go-v2/feature/dynamodb/expression v1.6.16
 	github.com/aws/aws-sdk-go-v2/service/dynamodb v1.27.0
+	github.com/danielgtaylor/huma/v2 v2.4.0
+	github.com/go-chi/chi/v5 v5.0.11
 	github.com/google/uuid v1.6.0
 	github.com/joeshaw/envdecode v0.0.0-20200121155833-099f1fc765bd
 	github.com/joho/godotenv v1.5.1
@@ -31,9 +33,16 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/sts v1.26.7 // indirect
 	github.com/aws/smithy-go v1.19.0 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/danielgtaylor/casing v1.0.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/fxamacker/cbor/v2 v2.5.0 // indirect
+	github.com/go-chi/chi v4.1.2+incompatible // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/spf13/cobra v1.8.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/x448/float16 v0.8.4 // indirect
 	golang.org/x/sys v0.16.0 // indirect
 )
